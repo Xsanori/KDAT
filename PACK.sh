aa4816bdb73e672829b22ba2268ff5dff16aa458
El'Xsanori Leo <39676248+Xsanori@users.noreply.github.com>
2019-06-20 16:37:44 +0300

    Add files via upload

---
+++ PACK.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Enter system size in bytes:"
+read XSSZ
+./make_ext4fs -T 0 -S file_contexts -l $(XSSZ) -a system system_new.img system/
+rm -rf system.transfer.list
+rm -rf system.new.dat
+./img2simg system_new.img system_sparse.img
+./img2sdat.py system_sparse.img
+sudo umount system/
+sudo rm -rf system/
---
+++ UNPACK.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./sdat2img.py system.transfer.list system.new.dat system.img
+mkdir system/
+sudo mount -t ext4 -o loop system.img system/
+XNME="$(whoami)"
+sudo chown -R $(XNME):$(XNME) $(pwd)/system/
